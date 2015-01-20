408c8f60edaa5530f080bb4b4015750db8bb1d9f
lww2578 <lww2578@naver.com>
2015-01-21 04:52:05 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,32 @@
 <?php
 
 	//세션을 사용하기 위해 선언하는 부분
-
 	session_cache_limiter('');
 
 	//데이터베이스에 접근하기 위한 부분
 
-	$dbid = "root";
+$db_host="localhost";
 
-	$dbpass = "autoset";
+$db_user="root";
 
-	$dbname ="project2";
+$db_pass="autoset";
 
-	$dbhost = "localhost";
+$db_name="project2";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+/*if(mysqli_connect_errno($conn))
+{
+
+echo "database connect failed";
+
+}else
+{
+
+echo "database connect success";
+
+}*/
+
 
 	//아이디와 비밀번호의 값을 POST방식으로 받는 것
 
@@ -24,11 +38,11 @@
 
 	//입력받은 아이디가 존재하는지 체크하기 위해 데이터베이스에서 id를 가져옴
 
-	$getID = "SELECT id FROM MEMBERS WHERE id='$id'";
+	$getID = "SELECT id FROM id_list WHERE id='$id'";
 
-	$getID = new mysql_query($getID);
-
-	$getID = new mysql_fetch_array($getID);
+	$getID = mysqli_query($conn,$getID);
+    
+	$getID = mysqli_fetch_array($getID, MYSQLI_ASSOC);
 
 	
 
@@ -38,27 +52,24 @@
 
 		//아이디를 바탕으로 그 아이디가 가진 곳의 비밀번호를 가져온다
 
-		$getPASS = "SELECT password FROM MEMBERS WHERE id='$id'";
-
-		$getPASS = mysql_query($getPASS);
-
-		$getPASS = mysql_result($getPASS, 0);
-
-		
-
+		$getPASS = "SELECT password FROM id_list WHERE id='$id'";
+		$getPASS = mysqli_query($conn,$getPASS);
+       
+		$getPASS = mysqli_fetch_array($getPASS, MYSQLI_ASSOC);
 		//데이터베이스에서 가져온 비밀번호가 입력받은 비밀번호와 같다면,
 
-		if($getPASS == $pass) {
+		if($getPASS['password'] == $pass) {
 
 			//64자리의 무작위 문자열을 생성한다.
 
 			//이 64자리의 임의의 수가 바로 토큰으로 로그인 대조에 사용할 키 값.
+            header("location: ./main/main.php");
 		}
 
 		else {
 
-			echo "PASSWORD ERROR";
-
+			echo "<script>alert('패스워드가 다릅니다.');</script>";
+            echo "<script>history.back();</script>";
 			return 1;
 
 		}
@@ -69,10 +80,11 @@
 
 	else {
 
-		echo "ID DOESN'T EXIST";
-
+		echo "<script>alert('아이디가 존재하지 않습니다.');</script>";
+        echo "<script>history.back();</script>";
 		return 1;
 
 	}
+
 
 ?>
